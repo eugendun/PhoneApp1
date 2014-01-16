64c5598cbefdb7ab670d9fbb36fb844b76a317e0
Eugen Dundukov <eugendun@gmail.com>
2014-01-16 23:58:41 +0100

    last

--- PhoneApp1/Pages/MembersPage.xaml.cs
+++ PhoneApp1/Pages/MembersPage.xaml.cs
@@ -7,19 +7,34 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using PhoneApp1.Models;
 
 namespace PhoneApp1.Views
 {
     public partial class MembersPage : PhoneApplicationPage
     {
+        private Member selectedMember;
+
         public MembersPage() {
             InitializeComponent();
             DataContext = App.ViewModel;
         }
 
-        private void OnClick_AddMemberButton(object sender, System.EventArgs e)
-        {
-        	NavigationService.Navigate(new Uri("/Pages/MemberNewPage.xaml", UriKind.Relative));
+        private void OnClick_AddMemberButton(object sender, System.EventArgs e) {
+            NavigationService.Navigate(new Uri("/Pages/MemberNewPage.xaml", UriKind.Relative));
+        }
+
+        private void DeleteMemberButton_Click(object sender, EventArgs e) {
+            if (selectedMember != null) {
+                App.ViewModel.Members.Remove(selectedMember);
+                App.ViewModel.phoneAppDB.SubmitChanges();
+            }
+        }
+
+        private void MemberList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var selector = sender as LongListSelector;
+            var selMember = selector.SelectedItem as Member;
+            selectedMember = selMember;
         }
     }
 }
